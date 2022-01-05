cdcb90bb322b39b7e4870a393af62b867f16b345
SourceOfPhi <phis_games@gmx.de>
2022-01-05 16:29:11 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import pygame
+from abc import ABC, abstractmethod
+from pathlib import Path
+
+# --- global constants ---
+SCREEN_SIZE = (800, 400)
+
+
+class Grid():
+    def __init__(self, cell_width: int) -> None:
+        self.cell_width = cell_width
+        self.cell_cnt_x = int(SCREEN_SIZE[0] / cell_width)
+        self.cell_cnt_y = int(SCREEN_SIZE[1] / cell_width)
+
+    def pos_to_cell(self, pos: tuple[int, int]) -> tuple[int, int]:
+        return (int(pos[0] / self.cell_width), int(pos[1] / self.cell_width))
+
+    def cell_to_pos(self, cell_pos: tuple[int, int]) -> tuple[int, int]:
+        return(cell_pos[0] * self.cell_width, cell_pos[1] * self.cell_width)
+
+    def draw(self, screen: pygame.Surface):
+        for y in range(self.cell_cnt_y):
+            for x in range(self.cell_cnt_x):
+                cell_pos = (x * self.cell_width, y * self.cell_width)
+                pygame.draw.rect(screen, (0, 0, 0),
+                                 pygame.Rect(cell_pos, (self.cell_width, self.cell_width)), width=1)
+
+
+class GameObject(ABC):
+    @abstractmethod
+    def draw(self, screen: pygame.Surface):
+        ...
+
+
+class Turret(GameObject):
+    def __init__(self, img: pygame.Surface, init_cell_pos: tuple[int, int], grid: Grid):
+        self.img = pygame.transform.scale(
+            img, (grid.cell_width, grid.cell_width))
+        self.cell_pos = init_cell_pos
+        self.pos = grid.cell_to_pos(self.cell_pos)
+
+    def draw(self, screen: pygame.Surface):
+        screen.blit(self.img, self.pos)
+
+
+def draw(screen: pygame.Surface, game_objects: list[GameObject]):
+    for go in game_objects:
+        go.draw(screen)
+
+
+def main():
+    # initialize the pygame module
+    pygame.init()
+
+    grid = Grid(cell_width=50)
+
+    turret_sprite = pygame.image.load(Path('./images/turretPlaceholder.png'))
+
+    turret1 = Turret(turret_sprite, (100, 100), grid)
+
+    game_objects = []
+    game_objects.append(turret1)
+
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+
+    mouse_pos: tuple[int, int] = 0, 0
+
+    # define a variable to control the main loop
+    running = True
+    screen.fill((255, 255, 255))
+
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                mouse_pos_ingrid = grid.pos_to_cell(mouse_pos)
+                # TODO: if on this pos should be built
+                turret = Turret(turret_sprite, mouse_pos_ingrid, grid)
+                game_objects.append(turret)
+
+        # TODO: Update all game objects
+
+        # TODO: If enough time has passed to match the display frame rate
+        # Draw all game objects
+        grid.draw(screen)
+        draw(screen, game_objects)
+        pygame.display.update()
+        #draw(screen, game_objects)
+
+
+if __name__ == '__main__':
+    main()
