009f356f181651f153182885cfc931ae87a82d17
SourceOfPhi <phis_games@gmx.de>
2022-01-08 18:41:43 +0100

    Initialize enemy class

---
+++ enemy.py
@@ -0,0 +1,27 @@
+import pygame
+from game_object import GameObject
+
+class Enemy(GameObject):
+    DIST_THRESH = 0.1
+
+    def __init__(self, img: pygame.Surface, init_pos: tuple[int,int], path: list[tuple[int,int]]) -> None:
+        self.position: tuple[float,float] = (float(init_pos[0]), float(init_pos[1]))
+        self.path = path
+        self.curr_path_idx = 0
+        self.target = path[0]
+        self.speed: float = 0.1
+        self.img = img
+
+    def update(self):
+        dist = pygame.math.Vector2(self.position).distance_to(self.target)
+        print(dist)
+        if dist < Enemy.DIST_THRESH:
+            self.curr_path_idx = self.curr_path_idx + 1
+            if self.curr_path_idx > len(self.path):
+                print("Enemy reached end!")
+                return
+            self.target = self.path[self.curr_path_idx]
+        self.position = tuple(pygame.math.Vector2(self.position) + (pygame.math.Vector2(self.target) - pygame.math.Vector2(self.position)).normalize() * self.speed)
+
+    def draw(self, screen: pygame.Surface):
+        screen.blit(self.img, self.position)
