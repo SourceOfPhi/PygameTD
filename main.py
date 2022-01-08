41c19a4c2e10ca49b022fe21280108ec5ca7da50
SourceOfPhi <phis_games@gmx.de>
2022-01-08 19:20:21 +0100

    Remove grid from main

--- main.py
+++ main.py
@@ -1,53 +1,12 @@
 import pygame
 from pathlib import Path
 from enemy import Enemy
+from grid import Grid
 from levelparser import Level, LevelParser
 from game_object import GameObject
 
 # --- global constants ---
 SCREEN_SIZE = (800, 400)
-
-
-class Grid():
-    cell_width: int = 50
-
-    def __init__(self, level: Level) -> None:
-        self.cell_cnt_x = level.col_cnt  # int(SCREEN_SIZE[0] / cell_width)
-        self.cell_cnt_y = level.row_cnt  # int(SCREEN_SIZE[1] / cell_width)
-
-        self.blocked_cells: list[tuple[int, int]] = level.blocked_positions
-        self.path_cells = level.blocked_positions
-        print(self.blocked_cells)
-
-        global SCREEN_SIZE
-        SCREEN_SIZE = (Grid.cell_width * self.cell_cnt_x,
-                       Grid.cell_width * self.cell_cnt_y)
-
-    @staticmethod
-    def pos_to_cell(pos: tuple[int, int]) -> tuple[int, int]:
-        return (int(pos[0] / Grid.cell_width), int(pos[1] / Grid.cell_width))
-
-    @staticmethod
-    def cell_to_pos(cell_pos: tuple[int, int]) -> tuple[int, int]:
-        return(cell_pos[0] * Grid.cell_width, cell_pos[1] * Grid.cell_width)
-
-    def block_on_grid(self, cell_pos: tuple[int, int]):
-        if not cell_pos in self.blocked_cells:
-            self.blocked_cells.append(cell_pos)
-
-    def is_blocked(self, cell_pos: tuple[int, int]):
-        return cell_pos in self.blocked_cells
-
-    def draw(self, screen: pygame.Surface):
-        for y in range(self.cell_cnt_y):
-            for x in range(self.cell_cnt_x):
-                width = 1
-                color = (0, 0, 0)
-                if (x, y) in self.path_cells:
-                    width = 0
-                    color = (150, 150, 150)
-                cell_pos = (x * Grid.cell_width, y * Grid.cell_width)
-                pygame.draw.rect(screen, color, pygame.Rect(cell_pos, (Grid.cell_width, Grid.cell_width)), width=width)
 
 
 class Turret(GameObject):
