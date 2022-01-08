ae4e2c5d6fb21d336fb836ba014b61468758ea3b
SourceOfPhi <phis_games@gmx.de>
2022-01-08 11:19:47 +0100

    Add update() to GameObject

--- game_object.py
+++ game_object.py
@@ -3,5 +3,9 @@ from abc import ABC, abstractmethod
 
 class GameObject(ABC):
     @abstractmethod
+    def update(self):
+        ...
+
+    @abstractmethod
     def draw(self, screen: pygame.Surface):
         ...
