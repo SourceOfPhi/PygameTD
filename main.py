ee81cc3dfdcca6827c06b0a8448fe6f681eff7b4
SourceOfPhi <phis_games@gmx.de>
2022-01-16 18:12:38 +0100

    Move turret to separate file

--- main.py
+++ main.py
@@ -7,25 +7,10 @@ from levelparser import Level, LevelParser
 from game_object import GameObject
 from custom_types import CellPosition
 from game_state import GameState
+from turret import Turret
 
 # --- global constants ---
 SCREEN_SIZE = (800, 400)
-
-
-class Turret(GameObject):
-    def __init__(self, img: pygame.Surface, init_cell_pos: CellPosition):
-        self.img = pygame.transform.scale(img, (Grid.cell_width, Grid.cell_width))
-        self.cell_pos = init_cell_pos
-        self.pos = Grid.cell_to_pos(self.cell_pos)
-
-    def draw(self, screen: pygame.Surface):
-        screen.blit(self.img, self.pos)
-
-    def update(self):
-        pass
-
-    def shall_be_removed(self) -> bool:
-        return False
 
 
 def draw(screen: pygame.Surface, game_objects: list[GameObject]):
---
+++ turret.py
@@ -0,0 +1,26 @@
+import imp
+import pygame
+from game_object import GameObject
+from custom_types import CellPosition
+from grid import Grid
+
+
+class Turret(GameObject):
+    def __init__(self, img: pygame.Surface, init_cell_pos: CellPosition):
+        self.img = pygame.transform.scale(img, (Grid.cell_width, Grid.cell_width))
+        self.cell_pos = init_cell_pos
+        self.pos = Grid.cell_to_pos(self.cell_pos)
+
+        self.cool_down_time = 1000  # ms
+        self.cool_down_current = self.cool_down_time
+
+    def draw(self, screen: pygame.Surface):
+        screen.blit(self.img, self.pos)
+
+    def update(self):
+        if self.cool_down_current > 0:
+            #self.cool_down_current = self.cool_down_current - pygame.time.get_ticks
+            pass
+
+    def shall_be_removed(self) -> bool:
+        return False
